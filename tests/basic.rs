3e30b64cda4bef21f0de3b3bcc7f2617414f87b2
OpenByte <development.openbyte@gmail.com>
2021-09-06 15:38:53 +0200

    Use unwrap instead of ? operator in test

--- tests/basic.rs
+++ tests/basic.rs
@@ -1,14 +1,18 @@
-use std::{error::Error, path::PathBuf, process::Command, str::FromStr};
+use std::{path::PathBuf, process::Command, str::FromStr};
 
 #[test]
-fn test() -> Result<(), Box<dyn Error>> {
-    let crate_path = PathBuf::from_str("tests")?.join("test-crate").canonicalize()?;
+fn test() {
+    let crate_path = PathBuf::from_str("tests")
+        .unwrap()
+        .join("test-crate")
+        .canonicalize()
+        .unwrap();
 
     Command::new("cargo")
         .arg("build")
         .current_dir(&crate_path)
-        .spawn()?
-        .wait()?;
-
-    Ok(())
+        .spawn()
+        .unwrap()
+        .wait()
+        .unwrap();
 }
